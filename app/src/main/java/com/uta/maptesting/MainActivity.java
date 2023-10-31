7582adf2175cc99d50006b1ba8fb08895bd794b9
mgm67671 <mgm0489@mavs.uta.edu>
2023-10-31 13:44:51 -0500

    Boundaries and Search

--- app/src/main/java/com/uta/maptesting/MainActivity.java
+++ app/src/main/java/com/uta/maptesting/MainActivity.java
@@ -1,48 +1,118 @@
 package com.uta.maptesting;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.MapStyleOptions;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 {
-//    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-//            new ActivityResultContracts.StartActivityForResult(),
-//            intent -> {
-//            });
+    private GoogleMap mMap;
+
+
+
+    // creating a variable
+    // for search view.
+    SearchView searchView;
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap)
-    {
-        googleMap.setMapStyle
-        (
-            MapStyleOptions.loadRawResourceStyle
-            (
-    this,
-                R.raw.theme
-            )
-        );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-        LatLng UTA = new LatLng(32.731636,-97.111128);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(UTA));
-        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        // initializing our search view.
+        searchView = findViewById(R.id.idSearchView);
+
+        // Obtain the SupportMapFragment and get notified
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
+
+        // adding on query listener for our search view.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // on below line we are getting the
+                // location name from search view.
+                String location = searchView.getQuery().toString();
+
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                List<Address> addressList = null;
+
+                // checking if the entered location is null or not.
+                // on below line we are creating and initializing a geo coder.
+                Geocoder geocoder = new Geocoder(MainActivity.this);
+                try {
+                    // on below line we are getting location from the
+                    // location name and adding that location to address list.
+                    addressList = geocoder.getFromLocationName(location, 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // on below line we are getting the location
+                // from our list a first position.
+                Address address = Objects.requireNonNull(addressList).get(0);
+
+                // on below line we are creating a variable for our location
+                // where we will add our locations latitude and longitude.
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                // on below line we are adding marker to that position.
+                mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+
+                // below line is to animate camera to that position.
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        // at last we calling our map fragment to update.
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.setMapStyle
+                (
+                        MapStyleOptions.loadRawResourceStyle
+                                (
+                                        this,
+                                        R.raw.theme
+                                )
+                );
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-        Objects.requireNonNull(mapFragment).getMapAsync(this);
+        /*LatLng UTA = new LatLng(32.731636, -97.111128);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(UTA));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15));*/
+
+        LatLngBounds UTAbounds = new LatLngBounds(
+                new LatLng(32.721262, -97.131970), // SW bounds
+                new LatLng(32.735551, -97.107085)  // NE bounds
+        );
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UTAbounds.getCenter(), 15));
+
+        googleMap.setLatLngBoundsForCameraTarget(UTAbounds);
     }
 }
