5e079a31daea203f75b002fb28bb679a2bf5ffad
elliotmai <145508224+elliotmai@users.noreply.github.com>
2023-11-17 18:10:37 -0600

    Markers V1.2
    Added parking and venues

--- app/src/main/java/com/uta/maptesting/MainActivity.java
+++ app/src/main/java/com/uta/maptesting/MainActivity.java
@@ -477,6 +477,406 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(building72)
                 .title("Woolf Hall - WH"));
 
+        LatLng parking1 = new LatLng(32.73323979, -97.11843252);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking1)
+                .title("Accessible Parking"));
+
+        LatLng parking2 = new LatLng(32.73237338, -97.11068094);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking2)
+                .title("F11 Visitor Parking"));
+
+        LatLng parking3 = new LatLng(32.73069, -97.126391);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking3)
+                .title("Maverick Stadium"));
+
+        LatLng parking4 = new LatLng(32.731877, -97.12059975);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking4)
+                .title("Meadow Run General Parking"));
+
+        LatLng parking5 = new LatLng(32.73352, -97.117231);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking5)
+                .title("Park West Parking Garage"));
+
+        LatLng parking6 = new LatLng(32.72922, -97.125163);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking6)
+                .title("Parking and Transportation Parking Lot"));
+
+        LatLng parking7 = new LatLng(32.729851, -97.124376);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking7)
+                .title("Parking Lot 27"));
+
+        LatLng parking8 = new LatLng(32.73356, -97.12197);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking8)
+                .title("Parking Lot 28"));
+
+        LatLng parking9 = new LatLng(32.72984, -97.12264);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking9)
+                .title("Parking Lot 29"));
+
+        LatLng parking10 = new LatLng(32.73119, -97.11958);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking10)
+                .title("Parking Lot 30"));
+
+        LatLng parking11 = new LatLng(32.73306, -97.12157);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking11)
+                .title("Parking Lot 31"));
+
+        LatLng parking12 = new LatLng(32.73339322, -97.11583614);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking12)
+                .title("Parking Lot 34 (student)"));
+
+        LatLng parking13 = new LatLng(32.73409, -97.11544);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking13)
+                .title("Parking Lot 35"));
+
+        LatLng parking14 = new LatLng(32.7345, -97.11315);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking14)
+                .title("Parking Lot 36"));
+
+        LatLng parking15 = new LatLng(32.732599, -97.109571);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking15)
+                .title("Parking Lot 38"));
+
+        LatLng parking16 = new LatLng(32.732563, -97.10897);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking16)
+                .title("Parking Lot 39"));
+
+        LatLng parking17 = new LatLng(32.72853761, -97.1075213);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking17)
+                .title("Parking Lot 45"));
+
+        LatLng parking18 = new LatLng(32.727256, -97.108047);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking18)
+                .title("Parking Lot 46"));
+
+        LatLng parking19 = new LatLng(32.72606, -97.11284);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking19)
+                .title("Parking Lot 49"));
+
+        LatLng parking20 = new LatLng(32.7246, -97.11238);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking20)
+                .title("Parking Lot 50"));
+
+        LatLng parking21 = new LatLng(32.72322, -97.11049);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking21)
+                .title("Parking Lot 51"));
+
+        LatLng parking22 = new LatLng(32.72571, -97.11049);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking22)
+                .title("Parking Lot 52"));
+
+        LatLng parking23 = new LatLng(32.72696, -97.10913);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking23)
+                .title("Parking Lot 53"));
+
+        LatLng parking24 = new LatLng(32.72799, -97.10735);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking24)
+                .title("Parking Lot 55"));
+
+        LatLng parking25 = new LatLng(32.72503, -97.10828);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking25)
+                .title("Parking Lot 56"));
+
+        LatLng parking26 = new LatLng(32.728772, -97.110418);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking26)
+                .title("Parking Lot F10"));
+
+        LatLng parking27 = new LatLng(32.73269829, -97.11038053);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking27)
+                .title("Parking Lot F11"));
+
+        LatLng parking28 = new LatLng(32.733, -97.11148);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking28)
+                .title("Parking Lot F12"));
+
+        LatLng parking29 = new LatLng(32.729914, -97.10919);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking29)
+                .title("Parking Lot F13"));
+
+        LatLng parking30 = new LatLng(32.733326, -97.11412);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking30)
+                .title("Parking Lot F14"));
+
+        LatLng parking31 = new LatLng(32.72627, -97.10822);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking31)
+                .title("Parking Lot F17"));
+
+        LatLng parking32 = new LatLng(32.73247, -97.12188);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking32)
+                .title("Parking Lot F4"));
+
+        LatLng parking33 = new LatLng(32.73399, -97.12035);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking33)
+                .title("Parking Lot F5"));
+
+        LatLng parking34 = new LatLng(32.73214, -97.11499);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking34)
+                .title("Parking Lot F7"));
+
+        LatLng parking35 = new LatLng(32.73106, -97.11682);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking35)
+                .title("Parking Lot F8"));
+
+        LatLng parking36 = new LatLng(32.72874, -97.11568);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking36)
+                .title("Parking Lot F9"));
+
+        LatLng parking37 = new LatLng(32.73147, -97.12251);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking37)
+                .title("Parking Lot GR"));
+
+        LatLng parking38 = new LatLng(32.726122, -97.118165);
+        mMap.addMarker(new MarkerOptions()
+                .position(parking38)
+                .title("Student & Administration Parking Lot"));
+
+        LatLng venue1 = new LatLng(32.72249, -97.129295);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue1)
+                .title("Allan Saxe Softball Field"));
+
+        LatLng venue2 = new LatLng(32.731265, -97.116128);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue2)
+                .title("Architecture Courtyard"));
+
+        LatLng venue3 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue3)
+                .title("Bluebonnet Ballroom"));
+
+        LatLng venue4 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue4)
+                .title("Carlisle Suite"));
+
+        LatLng venue5 = new LatLng(32.730801, -97.111507);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue5)
+                .title("Carolyn A Barros Reading Room"));
+
+        LatLng venue6 = new LatLng(32.722201, -97.130803);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue6)
+                .title("Clay Gould Ballpark"));
+
+        LatLng venue7 = new LatLng(32.730187, -97.108181);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue7)
+                .title("College Park Center"));
+
+        LatLng venue8 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue8)
+                .title("Concho Room"));
+
+        LatLng venue9 = new LatLng(32.731223, -97.107478);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue9)
+                .title("Dan Dipert University Welcome Center"));
+
+        LatLng venue10 = new LatLng(32.73217483, -97.11328268);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue10)
+                .title("Engineering Mall"));
+
+        LatLng venue11 = new LatLng(32.730577, -97.115085);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue11)
+                .title("Fine Arts Room 148"));
+
+        LatLng venue12 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue12)
+                .title("Guadalupe Room"));
+
+        LatLng venue13 = new LatLng(32.73093, -97.117653);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue13)
+                .title("Indoor Pool"));
+
+        LatLng venue14 = new LatLng(32.730577, -97.115085);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue14)
+                .title("Irons Recital Hall"));
+
+        LatLng venue15 = new LatLng(32.72966, -97.112687);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue15)
+                .title("Library Atrium"));
+
+        LatLng venue16 = new LatLng(32.72966, -97.112687);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue16)
+                .title("Library Parlor"));
+
+        LatLng venue17 = new LatLng(32.731925, -97.11705);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue17)
+                .title("Lone Star Auditorium"));
+
+        LatLng venue18 = new LatLng(32.73121039, -97.11486452);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue18)
+                .title("Mainstage Theatre"));
+
+        LatLng venue19 = new LatLng(32.73196499, -97.10753202);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue19)
+                .title("Maverick Pantry"));
+
+        LatLng venue20 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue20)
+                .title("Neches Room"));
+
+        LatLng venue21 = new LatLng(32.73093, -97.117653);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue21)
+                .title("Outdoor Pool"));
+
+        LatLng venue22 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue22)
+                .title("Palo Duro Lounge"));
+
+        LatLng venue23 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue23)
+                .title("Palo Pinto Room"));
+
+        LatLng venue24 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue24)
+                .title("Pedernales Room"));
+
+        LatLng venue25 = new LatLng(32.730396, -97.111744);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue25)
+                .title("Planetarium"));
+
+        LatLng venue26 = new LatLng(32.732585, -97.113819);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue26)
+                .title("Rady Room"));
+
+        LatLng venue27 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue27)
+                .title("Red River Room"));
+
+        LatLng venue28 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue28)
+                .title("Rio Grande Ballroom"));
+
+        LatLng venue29 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue29)
+                .title("Rosebud Theater"));
+
+        LatLng venue30 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue30)
+                .title("San Jacinto Room"));
+
+        LatLng venue31 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue31)
+                .title("San Saba Room"));
+
+        LatLng venue32 = new LatLng(32.732585, -97.113819);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue32)
+                .title("Science and Engineering Library"));
+
+        LatLng venue33 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue33)
+                .title("Sierras Lounge"));
+
+        LatLng venue34 = new LatLng(32.72764243, -97.11166903);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue34)
+                .title("Smart Hospital"));
+
+        LatLng venue35 = new LatLng(32.72966, -97.112687);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue35)
+                .title("Special Collections"));
+
+        LatLng venue36 = new LatLng(32.730577, -97.115085);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue36)
+                .title("Studio Theatre"));
+
+        LatLng venue37 = new LatLng(32.729723, -97.115497);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue37)
+                .title("Texas Hall"));
+
+        LatLng venue38 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue38)
+                .title("The Gallery at UC"));
+
+        LatLng venue39 = new LatLng(32.731457, -97.115053);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue39)
+                .title("The Gallery at UTA"));
+
+        LatLng venue40 = new LatLng(32.729404, -97.107291);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue40)
+                .title("The Green at College Park"));
+
+        LatLng venue41 = new LatLng(32.731544, -97.110941);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue41)
+                .title("University Center Mall"));
+
+        LatLng venue42 = new LatLng(32.731265, -97.116128);
+        mMap.addMarker(new MarkerOptions()
+                .position(venue42)
+                .title("West Campus Library"));
+
 
     }
 }
